095e70f70d8ce795e140fd234a36cff195c06306
MertArar <mertarar.ma@gmail.com>
2024-07-18 05:41:07 +0300

    Death anim added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,25 +1,38 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Animator animator;
-    private Rigidbody rigidbody;
+    [SerializeField] private float playerSpeed = 1.5f;
+    [SerializeField] private GameObject player;
 
-    public int jumpForce = 3;
-    [SerializeField] public float playerSpeed = 2.5f;
+    private Animator animator;
+    private Rigidbody rb;
+     
+    public static int currentTile = 0;
     
+    private int next_x_pos;
+    private float speedIncreaseInterval = 15f; 
+    private float speedIncreaseAmount = 0.2f; 
+    private float maxSpeed = 10f;
+    private bool canMove = true;
+    private bool Left, Right;
+    private bool isDead = false;
+
     void Start()
     {
         animator = GetComponent<Animator>();
-        rigidbody = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.W))
+        if (isDead) return; 
+
+        if (Input.GetKeyUp(KeyCode.Space))
         {
             animator.SetBool("Run", true);
         }
@@ -27,17 +40,95 @@ public class PlayerMovement : MonoBehaviour
         {
             animator.SetBool("Slide", true);
         }
-        else if (Input.GetKeyUp(KeyCode.Space))
+        else if (Input.GetKeyUp(KeyCode.W))
         {
             animator.SetBool("Jump", true);
         }
-        else if (Input.GetKeyUp(KeyCode.D))
+        else if (Input.GetKeyUp(KeyCode.D) && canMove == true)
         {
-            animator.SetBool("Right", true);
+            if (!animator.GetBool("Jump") && !animator.GetBool("Slide"))
+                animator.SetBool("Right", true);
+            else
+                Right = true;
+
+            if (rb.position.x >= -3 && rb.position.x < -1)
+            {
+                next_x_pos = 0;
+            }
+            else if (rb.position.x >= -1 && rb.position.x < 1)
+            {
+                next_x_pos = 2;
+            }
+
+            StartCoroutine(ToRight(next_x_pos));
+
+        }
+        else if (Input.GetKeyUp(KeyCode.A) && canMove == true)
+        {
+            if (!animator.GetBool("Jump") && !animator.GetBool("Slide"))
+                animator.SetBool("Left", true);
+            else
+                Left = true;
+            if (rb.position.x >= 1 && rb.position.x < 3)
+            {
+                next_x_pos = 0;
+            }
+            else if (rb.position.x >= -1 && rb.position.x < 1)
+            {
+                next_x_pos = -2;
+                
+            }
+
+            
+            StartCoroutine(ToLeft(next_x_pos));
+            
         }
     }
+    IEnumerator ToLeft(int next_x_pos)
+    {
+        canMove = false;
 
-    //Animation Events
+        float timer = 0.0125f;
+        yield return new WaitForSeconds(timer);
+        transform.position = new Vector3(this.next_x_pos + 0.8f, transform.position.y, transform.position.z);
+        yield return new WaitForSeconds(timer);
+
+        transform.position = new Vector3(this.next_x_pos + 0.6f, transform.position.y, transform.position.z);
+        yield return new WaitForSeconds(timer);
+
+        transform.position = new Vector3(this.next_x_pos + 0.4f, transform.position.y, transform.position.z);
+        yield return new WaitForSeconds(timer);
+
+        transform.position = new Vector3(this.next_x_pos + 0.2f, transform.position.y, transform.position.z);
+        yield return new WaitForSeconds(timer);
+
+        transform.position = new Vector3(this.next_x_pos, transform.position.y, transform.position.z + (playerSpeed/625)*100);
+
+        canMove = true;
+    }
+    IEnumerator ToRight(int next_x_pos)
+    {
+        canMove = false;
+
+        float timer = 0.0125f;
+        yield return new WaitForSeconds(timer);
+        transform.position = new Vector3(this.next_x_pos - 0.8f, transform.position.y, transform.position.z);
+        yield return new WaitForSeconds(timer);
+
+        transform.position = new Vector3(this.next_x_pos - 0.6f, transform.position.y, transform.position.z);
+        yield return new WaitForSeconds(timer);
+
+        transform.position = new Vector3(this.next_x_pos - 0.4f, transform.position.y, transform.position.z);
+        yield return new WaitForSeconds(timer);
+
+        transform.position = new Vector3(this.next_x_pos - 0.2f, transform.position.y, transform.position.z);
+        yield return new WaitForSeconds(timer);
+
+        transform.position = new Vector3(this.next_x_pos, transform.position.y, transform.position.z + (playerSpeed / 625)*100);
+
+        canMove = true;
+    }
+    //Animation events
     void ToggleOff(string Name)
     {
         animator.SetBool(Name, false);
@@ -45,35 +136,86 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private bool isJumpDown = false;
-    
+
     void JumpDown()
     {
         isJumpDown = true;
     }
-    
+
     private void OnAnimatorMove()
     {
+        if (isDead) return; 
+
         if (animator.GetBool("Jump"))
         {
             if (isJumpDown)
-            {
-                rigidbody.MovePosition(rigidbody.position + new Vector3(0,0,0f) * animator.deltaPosition.magnitude);
-            }
+                rb.MovePosition(rb.position + new Vector3(0, 0, 0) * animator.deltaPosition.magnitude);
             else
-            {
-                rigidbody.MovePosition(rigidbody.position + new Vector3(0,1.5f,0f) * animator.deltaPosition.magnitude);
-            }
-            
+                rb.MovePosition(rb.position + new Vector3(0, 1.5f, 0) * animator.deltaPosition.magnitude);
         }
+        else if (animator.GetBool("Right"))
+        {
+            if (rb.position.x < next_x_pos)
+                rb.MovePosition(rb.position + new Vector3(1, 0, 1.5f) * animator.deltaPosition.magnitude);
+            else
+                animator.SetBool("Right", false);
+        }
+        else if (animator.GetBool("Left"))
+        {
+            if (rb.position.x > next_x_pos)
+                rb.MovePosition(rb.position + new Vector3(-1, 0, 1.5f) * animator.deltaPosition.magnitude);
+            else
+                animator.SetBool("Left", false);
+        }
+        
         else
-           rigidbody.MovePosition(rigidbody.position + Vector3.forward * animator.deltaPosition.magnitude * playerSpeed);
+        {
+            float currentSpeed = Mathf.Min(playerSpeed, maxSpeed);
+            rb.MovePosition(rb.position + Vector3.forward * animator.deltaPosition.magnitude * currentSpeed);
+        }
+
+        if (Left)
+        {
+            if (rb.position.x > next_x_pos)
+                rb.MovePosition(rb.position + new Vector3(-1, 0, 0) * animator.deltaPosition.magnitude);
+            else
+                Left = false;
+        }
+
+        else if (Right)
+        {
+            if (rb.position.x < next_x_pos)
+                rb.MovePosition(rb.position + new Vector3(1, 0, 0) * animator.deltaPosition.magnitude);
+            else
+                Right = false;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Obs")
+        if (collision.collider.CompareTag("Obs"))
         {
-            
+            animator.SetBool("Dead", true);
+            collision.collider.enabled = false; 
+            isDead = true;
+            StartCoroutine(MoveBackwardOnDeath());
         }
+    }
+
+    private IEnumerator MoveBackwardOnDeath()
+    {
+        float moveTime = 0.5f;
+        float elapsedTime = 0f;
+        Vector3 startPos = transform.position;
+        Vector3 endPos = new Vector3(transform.position.x, transform.position.y, transform.position.z - 2);
+
+        while (elapsedTime < moveTime)
+        {
+            transform.position = Vector3.Lerp(startPos, endPos, (elapsedTime / moveTime));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = endPos;
     }
 }
